416f4985a5fddec32855c922467415118fcf1186
Damjan Kotevski <damjankotevski@hotmail.com>
2026-05-29 18:20:33 +0200

    Fix user routes controller references

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,12 +4,14 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
-router.post("/register", userController.registerUser);
+router.post(
+    "/register",
+    userController.registerUser
+);
 
-router.post("/login", userController.loginUser);
-
-router.get("/", userController.getAllUsers);
-
-router.delete("/:id", userController.deleteUser);
+router.post(
+    "/login",
+    userController.loginUser
+);
 
 module.exports = router;
